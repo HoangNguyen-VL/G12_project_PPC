32edfcc93e9df860d1c85d0bc2f06d688c43e9b7
VinhLy0803 <151431869+VinhLy0803@users.noreply.github.com>
2023-11-25 12:57:05 +0700

    Add files via upload

---
+++ triggerProperty.sql
@@ -0,0 +1,11 @@
+﻿CREATE TRIGGER after_insert_property
+	ON dbo.Property
+	AFTER INSERT 
+AS BEGIN
+	SET NOCOUNT ON;
+    
+    UPDATE Property
+    SET property_code = CONCAT('BĐS-', RIGHT('00000' + CAST(Property.Id AS VARCHAR(5)), 5))
+    FROM inserted
+    WHERE Property.Id = inserted.Id;
+END
